51a94976b22712e377a29364f2607232b690f4f1
shiwei <401181980@qq.com>
2024-02-26 11:48:37 +0800

    write Category files done v1

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,16 +1,101 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
 import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
 @Slf4j
+@Api(tags = "category relevant interface")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * add category
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("add category")
+    public Result<String> add(@RequestBody CategoryDTO categoryDTO) {
+        log.info("add category: {}", categoryDTO);
+        categoryService.add(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * category query by pages
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("category pages")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("pages query: {}", categoryPageQueryDTO);
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * delete category
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("delete category")
+    public Result deleteById(Long id) {
+        log.info("delete category: {}", id);
+        categoryService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * update category
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("update category")
+    public Result<String> update(@RequestBody CategoryDTO categoryDTO) {
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * able or disable category status
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("able or disable status")
+    public Result ableOrDisable(@PathVariable("status") Integer status, Long id) {
+        log.info("update id:{} category status:{}", id, status);
+        categoryService.update(status, id);
+        return Result.success();
+    }
+
+    /**
+     * get category by type
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("get category by type")
+    public Result<List<Category>> list(Integer type) {
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
 }
--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,7 +1,29 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
+    @Insert("Insert Into sky_take_out.category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+            "values " +
+            "(#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    void insert(Category category);
+
+
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    @Delete("DELETE from sky_take_out.category where id = #{id}")
+    void deleteById(Long id);
+
+    void update(Category category);
+
+
+    List<Category> list(Integer type);
 }
